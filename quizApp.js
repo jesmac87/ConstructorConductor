36cd5e693ce3c2abb2e7ea31f594c9bf4e869128
Jesse Mace <jesmac87@gmail.com>
2015-01-28 20:16:56 -0700

    Finish quizApp.js

--- quizApp.js
+++ quizApp.js
@@ -5,27 +5,43 @@
 
 //Create a User constructor that accepts name, email, password, and totalScore parameters and set them appropriatly
 
-  //code here
+var User = function (name, email, password, totalScore) {
+	this.name = name;
+	this.email = email;
+	this.password = password;
+	this.totalScore = totalScore;
+};
 
 
 //Create a Question constructor that accepts title, answersArray, rightAnswer, and difficulty parameters
 
-  //code here
+var Question = function	(title, answersArray, rightAnswer, difficulty) {
+	this.title = title;
+	this.answersArray = answersArray;
+	this.rightAnswer = rightAnswer;
+	this.difficulty = difficulty;
+};
 
 
 //Create a users Array which is going to hold all of our users.
 
-  //code here
+var users = [];
 
 
 //Let's say three people signed up for our service, create 3 instances of User and add each to the users Array
 
-  //code here
+var user1 = new User('jesse', 'rando@gmail.com', 'password123', 100);
+var user2 = new User('mike', 'hello@gmail.com', 'yahoo', 50);
+var user3 = new User('wendy', 'wendy@gmail.com', 'yolo', 75);
+
+users.push(user1);
+users.push(user2);
+users.push(user3);
 
 
 //Create a questions Array which is going to hold all of our questions
 
-  //code here
+var questions = [];
 
 
 //Now, let's say we wanted to create a quiz about JavaScript. Create three instances of Question which contain the following data
@@ -34,15 +50,28 @@
 //title: "T/F: In Javascript, == doesn't check 'type' but just the value - where === checks type and value"
 //Fill in the rest of the required data as you see appropriate.
 
-  //code here
-
+var question1 = new Question('T/F: Inheritance is achieved in JavaScript through Prototypes?', [true, false], true, 'Easy');
+var question2 = new Question('T/F: JavaScript is just a scripting version of Java', [true, false], false, 'Hard');
+var question3 = new Question("T/F: In Javascript, == doesn't check 'type' but just the value - where === checks type and value", [true,false], true, 'Difficult');
 
 //Now push all of your instances of Question into the questions Array
 
-  //code here
+questions.push(question1);
+questions.push(question2);
+questions.push(question3);
 
 console.log('My users Array and my questions arrray are ...');
 //Now loop console.log your users array and your questions array and verify that they're both holding the right data.
 
-  //code here
 
+
+var displayArrayObjectsElements = function (array) {
+	for (var i = 0; i < array.length; i++) {
+		for (var key in array[i]) {
+			console.log(key + ': ' + array[i][key]);
+		};
+	};
+};
+
+displayArrayObjectsElements(users);
+displayArrayObjectsElements(questions);
